2b569de40c12d13e58f648a39758062eb6b48b5c
webitech <radu.lainiceanu@gmail.com>
2020-09-09 21:16:45 +0300

    Initial

--- src/main/java/pieces/Piece.java
+++ src/main/java/pieces/Piece.java
@@ -4,4 +4,40 @@ public interface Piece {
     int[] move(int x, int y);
 
     boolean setWhite(boolean isWhite);
+
+    /*
+    public abstract class Piece {
+
+    private boolean killed = false;
+    private boolean white = false;
+
+    public Piece(boolean white)
+    {
+        this.setWhite(white);
+    }
+
+    public boolean isWhite()
+    {
+        return this.white == true;
+    }
+
+    public void setWhite(boolean white)
+    {
+        this.white = white;
+    }
+
+    public boolean isKilled()
+    {
+        return this.killed == true;
+    }
+
+    public void setKilled(boolean killed)
+    {
+        this.killed = killed;
+    }
+
+    public abstract boolean canMove(Board board,
+                                 Spot start, Spot end);
+}
+    * */
 }
