2941c39c23b7378191b584ee0c929d10a36cdd69
Gabriel Chiriac <61273744+shantajj@users.noreply.github.com>
2020-09-10 19:18:40 +0300

    Created Grid with Pane

--- src/main/java/graphic/GameLayout.java
+++ src/main/java/graphic/GameLayout.java
@@ -1,38 +1,68 @@
 package graphic;
 
 
-import javafx.scene.*;
+import javafx.geometry.Insets;
+import javafx.scene.Group;
+import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.Pane;
-import javafx.scene.layout.StackPane;
-import javafx.scene.shape.Rectangle;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
 
 
 public class GameLayout extends MainLayout {
 
     protected Scene gameStage() {
 
-        Pane paneGrid = new Pane();
-        boolean trigger = false;
-        
-        for (int i = 0; i < 8; i++){
-            for (int j = 0; j < 8; j++){
-                Rectangle rectangle = new Rectangle(i*100,j*100,100,100);
-                if (trigger){
-                    rectangle.setFill(Color.BLACK);
-                    trigger = !trigger;
-                }else {
-                    trigger = !trigger;
-                    rectangle.setFill(Color.WHITE);
+        GridPane gridPane = new GridPane();
+        final int size = 8;
+
+        for (int row = 0; row < size; row++) {
+            for (int col = 0; col < size; col++) {
+                Rectangle rectangle = new Rectangle();
+                Color color;
+                if ((row + col) % 2 == 0) {
+                    color = Color.WHITE;
+                } else {
+                    color = Color.BLACK;
                 }
-                paneGrid.getChildren().addAll(rectangle);
+                rectangle.setFill(color);
+                gridPane.add(rectangle, col, row);
+                rectangle.widthProperty().bind(gridPane.widthProperty().divide(size));
+                rectangle.heightProperty().bind(gridPane.heightProperty().divide(size));
             }
-            trigger = !trigger;
         }
+        Group group = new Group(gridPane);
+
+        gridPane.setId("borderGrid");
+        gridPane.getStylesheets().add("style.css");
+
+        BorderPane borderPane = new BorderPane();
+
+        HBox hBoxTop = new HBox();
+        hBoxTop.setMinHeight(50);
+//
+//        HBox hBoxBot = new HBox();
+//        hBoxBot.setMinHeight(100);
+//
+//        VBox vBoxRight = new VBox();
+//        vBoxRight.setMinWidth(100);
+//
+//        VBox vBoxLeft = new VBox();
+//        vBoxLeft.setMinWidth(100);
+
+        borderPane.setTop(hBoxTop);
+//        borderPane.setBottom(hBoxBot);
+//        borderPane.setRight(vBoxRight);
+//        borderPane.setLeft(vBoxLeft);
+        borderPane.setCenter(gridPane);
+
+        borderPane.setPadding(new Insets(0,250,50,250));
 
 
-        Scene scene = new Scene(paneGrid, MainLayout.initialSceneWidth, MainLayout.initialSceneHeight);
+        Scene scene = new Scene(borderPane, MainLayout.initialSceneWidth, MainLayout.initialSceneHeight);
+
 
         return scene;
     }
