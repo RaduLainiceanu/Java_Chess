adcb1a61706ba3f43d924205e3baa5cd0199bd31
VAca <office@webitech.ro>
2020-09-10 10:11:11 +0300

    awqwq

--- src/main/java/repository/Board.java
+++ src/main/java/repository/Board.java
@@ -1,5 +1,15 @@
 package repository;
 
 public class Board {
+    Spot[][] boxes;
 
+    public Board(Spot[][] boxes) {
+        this.boxes = boxes;
+    }
+    public Spot getBoxe(int x, int y) throws Exception {
+        if (x < 0 || x > 7 || y < 0 || y > 7){
+            throw new Exception("Index out of Bound");
+        }
+        return boxes[x][y];
+    }
 }
