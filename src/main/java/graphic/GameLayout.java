0ede91a05ce35f20df52c57abf62d229807641cf
Gabriel Chiriac <61273744+shantajj@users.noreply.github.com>
2020-09-10 23:15:39 +0300

    Working example of selection for indexes

--- src/main/java/graphic/GameLayout.java
+++ src/main/java/graphic/GameLayout.java
@@ -1,15 +1,12 @@
 package graphic;
 
-
+import javafx.geometry.HPos;
 import javafx.geometry.Insets;
+import javafx.geometry.VPos;
 import javafx.scene.Group;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.GridPane;
-import javafx.scene.layout.HBox;
-import javafx.scene.paint.Color;
-import javafx.scene.shape.Rectangle;
-
+import javafx.scene.control.Control;
+import javafx.scene.layout.*;
 
 public class GameLayout extends MainLayout {
 
@@ -17,53 +14,63 @@ public class GameLayout extends MainLayout {
 
         GridPane gridPane = new GridPane();
         final int size = 8;
+        double tileWidth = MainLayout.initialSceneWidth / size;
+        double tileHeight = MainLayout.initialSceneHeight / size;
+
 
         for (int row = 0; row < size; row++) {
             for (int col = 0; col < size; col++) {
-                Rectangle rectangle = new Rectangle();
-                Color color;
+                StackPane stackPane = new StackPane();
+                String color;
                 if ((row + col) % 2 == 0) {
-                    color = Color.WHITE;
+                    color = "white";
                 } else {
-                    color = Color.BLACK;
+                    color = "black";
                 }
-                rectangle.setFill(color);
-                gridPane.add(rectangle, col, row);
-                rectangle.widthProperty().bind(gridPane.widthProperty().divide(size));
-                rectangle.heightProperty().bind(gridPane.heightProperty().divide(size));
+                Tile tile = new Tile(row, col);
+                tile.setPrefSize(tileWidth, tileHeight);
+                tile.setStyle("-fx-background-color: " + color + ";");
+                gridPane.add(tile, col, row);
+
             }
         }
+        for (int i = 0; i < size; i++) {
+            gridPane.getColumnConstraints().add(new ColumnConstraints(5, Control.USE_COMPUTED_SIZE, Double.POSITIVE_INFINITY, Priority.ALWAYS, HPos.CENTER, true));
+            gridPane.getRowConstraints().add(new RowConstraints(5, Control.USE_COMPUTED_SIZE, Double.POSITIVE_INFINITY, Priority.ALWAYS, VPos.CENTER, true));
+        }
+
         Group group = new Group(gridPane);
 
-        gridPane.setId("borderGrid");
+        gridPane.setId("pane");
         gridPane.getStylesheets().add("style.css");
 
         BorderPane borderPane = new BorderPane();
 
         HBox hBoxTop = new HBox();
         hBoxTop.setMinHeight(50);
-//
-//        HBox hBoxBot = new HBox();
-//        hBoxBot.setMinHeight(100);
-//
-//        VBox vBoxRight = new VBox();
-//        vBoxRight.setMinWidth(100);
-//
-//        VBox vBoxLeft = new VBox();
-//        vBoxLeft.setMinWidth(100);
 
         borderPane.setTop(hBoxTop);
-//        borderPane.setBottom(hBoxBot);
-//        borderPane.setRight(vBoxRight);
-//        borderPane.setLeft(vBoxLeft);
+
         borderPane.setCenter(gridPane);
 
-        borderPane.setPadding(new Insets(0,250,50,250));
-
+        borderPane.setPadding(new Insets(0, 250, 50, 250));
 
         Scene scene = new Scene(borderPane, MainLayout.initialSceneWidth, MainLayout.initialSceneHeight);
 
-
         return scene;
     }
+
+    class Tile extends Pane {
+        private int posX;
+        private int posY;
+
+        public Tile(int x, int y) {
+            posX = x;
+            posY = y;
+            setOnMouseClicked(e -> {
+                System.out.println(posX + " " + posY);
+            });
+        }
+    }
+
 }
