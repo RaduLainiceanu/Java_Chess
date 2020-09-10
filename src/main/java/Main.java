80779b0bf49797236a78c0a5977c51614e5a3487
VAca <office@webitech.ro>
2020-09-10 13:00:55 +0300

    Update

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,8 +3,9 @@ import javafx.application.Application;
 
 
 public class Main {
-
     public static void main(String[] args) {
-        Application.launch(args);
+        Application.launch(Layout.class, args);
+
+
     }
 }
--- src/main/java/graphic/Layout.java
+++ src/main/java/graphic/Layout.java
@@ -1,24 +1,44 @@
 package graphic;
 
 import javafx.application.Application;
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.*;
 import javafx.stage.Stage;
 
+import java.awt.*;
+
 public class Layout extends Application {
+
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Button btn = new Button();
-        btn.setText("Say 'Hello World'");
-        btn.setOnAction(event -> System.out.println("Hello World"));
+        primaryStage.setTitle("CHESS");
+        primaryStage.getIcons().add(new Image("stage_icon.png"));
+        AnchorPane anchorPane = new AnchorPane();
 
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
+        Button singlePlayer = new Button();
+        singlePlayer.setPrefSize(195,195);
+        singlePlayer.setId("singleplayer");
+        singlePlayer.getStylesheets().add("style.css");
 
-        Scene scene = new Scene(root, 300, 250);
+        Button multiPlayer = new Button();
+        multiPlayer.setPrefSize(195,195);
+        multiPlayer.setId("multiplayer");
+        multiPlayer.getStylesheets().add("style.css");
 
-        primaryStage.setTitle("Hello World!");
+        Button options = new Button();
+        options.setPrefSize(195,195);
+        options.setId("options");
+        options.getStylesheets().add("style.css");
+
+        VBox vBox = new VBox(5);
+        vBox.getChildren().addAll(singlePlayer, multiPlayer, options);
+
+        anchorPane.getChildren().addAll(vBox);
+        Scene scene = new Scene(anchorPane, 800, 600);
         primaryStage.setScene(scene);
         primaryStage.show();
     }
