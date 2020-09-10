ed8584afab1beeeb1f99a4867b3b178f7faec06a
Gabriel Chiriac <61273744+shantajj@users.noreply.github.com>
2020-09-10 13:07:09 +0300

    Spacing update

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,7 +5,5 @@ import javafx.application.Application;
 public class Main {
     public static void main(String[] args) {
         Application.launch(Layout.class, args);
-
-
     }
 }
--- src/main/java/graphic/Layout.java
+++ src/main/java/graphic/Layout.java
@@ -5,11 +5,9 @@ import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
-import javafx.scene.layout.*;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
-
-import java.awt.*;
 
 public class Layout extends Application {
 
@@ -20,21 +18,22 @@ public class Layout extends Application {
         AnchorPane anchorPane = new AnchorPane();
 
         Button singlePlayer = new Button();
-        singlePlayer.setPrefSize(195,195);
+        singlePlayer.setPrefSize(190, 190);
         singlePlayer.setId("singleplayer");
         singlePlayer.getStylesheets().add("style.css");
 
         Button multiPlayer = new Button();
-        multiPlayer.setPrefSize(195,195);
+        multiPlayer.setPrefSize(190, 190);
         multiPlayer.setId("multiplayer");
         multiPlayer.getStylesheets().add("style.css");
 
         Button options = new Button();
-        options.setPrefSize(195,195);
+        options.setPrefSize(190, 190);
         options.setId("options");
         options.getStylesheets().add("style.css");
 
         VBox vBox = new VBox(5);
+        vBox.setPadding(new Insets(5,0,5,5));
         vBox.getChildren().addAll(singlePlayer, multiPlayer, options);
 
         anchorPane.getChildren().addAll(vBox);
